a3b9028e8bf604cc0b75a2d37474a561f35cd5c1
Iago Fernandes <iago.fernandes@atsinformatica.com.br>
2022-05-06 13:28:32 -0300

    Tela de Venda - Att

--- View/UVendaProdView.pas
+++ View/UVendaProdView.pas
@@ -4,13 +4,34 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, ExtCtrls, ComCtrls;
+  Dialogs, ExtCtrls, ComCtrls, DB, DBClient, Grids, DBGrids, StdCtrls,
+  Mask, Buttons;
 
 type
   TfrmVendaProd = class(TForm)
     StatusBar1: TStatusBar;
     pnlInfo: TPanel;
     pnlBotoes: TPanel;
+    btnConsultar: TBitBtn;
+    btnSair: TBitBtn;
+    btnPesquisar: TBitBtn;
+    btnAlterar: TBitBtn;
+    lblNumeroPedido: TLabel;
+    edtNumeroPedido: TEdit;
+    lblCliente: TLabel;
+    edtNumeroCliente: TEdit;
+    edtNomeCliente: TEdit;
+    Label1: TLabel;
+    edtValorTotal: TEdit;
+    lblData: TLabel;
+    MaskEdit1: TMaskEdit;
+    GroupBox1: TGroupBox;
+    DBGrid1: TDBGrid;
+    DataSource1: TDataSource;
+    ClientDataSet1: TClientDataSet;
+    btnConfirmar: TBitBtn;
+    btnCancelar: TBitBtn;
+    btnSalvar: TBitBtn;
   private
     { Private declarations }
   public
